431b60d339aa45b94538ccf619116d9ef05f49a8
douaa alouta <131913190+douaa23@users.noreply.github.com>
2023-08-23 14:20:47 +0200

    Create calculater.py

---
+++ calculater.py
@@ -0,0 +1,42 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Division by zero is not allowed"
+
+while True:
+    print("Options:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Quit")
+    
+    choice = input("Select an option (1/2/3/4/5): ")
+    
+    if choice == '5':
+        print("Thank you for using the calculator!")
+        break
+    
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid option")
